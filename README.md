9307ada2bd6ddeecee6b9d1d5033f883911e607e
fawzakin <thelastchoosen@gmail.com>
2021-09-22 21:24:54 +0700

    Add generate.sh

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Matcha Wallpaper (WIP)
-This repository contains wallpaper with the color scheme based on the Matcha theme. Both the original and my custom version are supported (dark theme only). Instruction coming soon.
+This repository contains wallpaper with the color scheme based on the Matcha theme. Both the original and my custom version are supported (dark theme only).
+
+### Instruction (WIP)
+Use "-h" to see all available option.
 
 # License and Credit
 CC0 v1.0 Universal. Credit coming soon.
--- generate.sh
+++ generate.sh
@@ -1,24 +1,99 @@
 #!/bin/bash
 
-# Check if you have Inkscape installed.
-ink=$(which inkscape) || { echo "Inkscape is not installed" && exit 1; }
+
+# Listing all variants
+ico_all=('arch' 'debian' 'fedora' 'gear' 'kubuntu' 'mint' 'pop' 'solus' 'ubuntu')
+bgr_all=('spot' 'stripe')
+col_all=('aliz' 'azul' 'sea' 'pueril' 'alice' 'azure' 'tea' 'amethyst')
+
+# Color list
+declare -A col_list
+col_list[aliz]="1a1a1a 222222 262626 2b2b2b 323232 d7d7d7 bc3f39 f0554c" 
+col_list[azul]="14161b 1b1d24 22252c 292b32 303238 d7d7d7 217dbb 3498db"
+col_list[sea]="141a1b 1b2224 222b2e 293032 303739 d7d7d7 00906f 2eb398"
+col_list[pueril]="1a1a1a 222222 262626 2b2b2b 323232 d7d7d7 6e8e4e 97bb72"
+col_list[alice]="1a1a1a 222222 262626 2b2b2b 323232 d7d7d7 a21b1b ef2929"
+col_list[azure]="14161b 1b1d24 22252c 292b32 303238 d7d7d7 23446e 3465a4"
+col_list[tea]="141a1b 1b2224 222b2e 293032 303739 d7d7d7 4e8e00 73d216"
+col_list[amethyst]="1a1a1a 222222 262626 2b2b2b 323232 d7d7d7 5a3a6a 75507b"
+
+# Print out help
+print_help() {
+printf "Available Options:\\n%s\\n%s\\n%s\\n%s\\n%s\\n%s\\n" \
+       "  -i: Icon variant (def: all icons)" \
+       "  -b: Background variant (def: all backgrounds)" \
+       "  -c: Color variant (def: all colors)" \
+       "  -n: Don't export all svg to pngs (def: export to png)" \
+       "  -o: Optimize exported pngs (def: not optimize)" \
+       "  -h: Show this message and exit"
+exit 0
+}
+
+# Main function: Generating our wallpapers
+export_color() {
+for i in ${ico_sel[@]:-${ico_all[@]}}; do for b in ${bgr_sel[@]:-${bgr_all[@]}}; do for c in ${col_sel[@]:-${col_all[@]}}; do
+
+    local name="Template/${i}-${b}.svg"
+    local col_val="${col_list[${c}]}"
+    local esvg="SVG/${i}-${b}-${c}.svg"
+    local epng="Matcha/${i}-${b}-${c}.png"
+
+    [ ! -f "${name}" ] && echo "${name} doesn't exist" && echo "" && break 
+    [ -z "${col_val}" ] && echo "${c} isn't defined for ${esvg}" && echo "" && continue
+
+    echo "Generating ${esvg}"
+    sed "s/000000/$(cut -d" " -f1 <<< ${col_val})/g;
+         s/101010/$(cut -d" " -f2 <<< ${col_val})/g;
+         s/202020/$(cut -d" " -f3 <<< ${col_val})/g;
+         s/303030/$(cut -d" " -f4 <<< ${col_val})/g;
+         s/404040/$(cut -d" " -f5 <<< ${col_val})/g;
+         s/ffffff/$(cut -d" " -f6 <<< ${col_val})/g;
+         s/aa0000/$(cut -d" " -f7 <<< ${col_val})/g;
+         s/ff0000/$(cut -d" " -f8 <<< ${col_val})/g" ${name} > ${esvg}
+
+    [ -z "${no_png}" ] && echo "Exporting ${epng}" && inkscape --export-filename=${epng} ${esvg} > /dev/null 2>&1
+    echo ""
+
+done; done; done
+}
+
+# Taking inputs
+while [ "$#" -gt 0 ]; do
+    case "${1}" in
+        -i) shift 1
+            for ico_in in "${@}"; do
+                [[ ${ico_in} = -* ]] && break
+                ico_sel+=("${ico_in}")
+                shift 1
+            done ;;
+        -b) shift 1
+            for bgr_in in "${@}"; do
+                [[ ${bgr_in} = -* ]] && break
+                bgr_sel+=("${bgr_in}")
+                shift 1
+            done ;;
+        -c) shift 1
+            for col_in in "${@}"; do
+                [[ ${col_in} = -* ]] && break
+                col_sel+=("${col_in}")
+                shift 1
+            done ;;
+        -n) no_png=1
+            shift 1;;
+        -o) if [ -x "$(command -v optipng)" ]; then opti=1; else echo "optipng is not installed"; exit 1; fi
+            shift 1;;
+        -h) print_help ;;
+    esac
+done
 
 # Create directories
 mkdir -p SVG Matcha
 
-# ---| Aliz |---
-for svg in Template/*.svg; do
-    exp="$(echo ${svg} | sed 's/Template/SVG/;s/\.svg/-dark-aliz.svg/')"
-    trg="$(echo ${svg} | sed 's/Template/Matcha/;s/\.svg/-dark-aliz.png/')"
-    echo "Exporting ${exp}"
-    cp -f ${svg} ${exp}
-    sed -i 's/000000/1a1a1a/g;
-            s/101010/222222/g;
-            s/202020/262626/g;
-            s/303030/2b2b2b/g;
-            s/404040/323232/g;
-            s/ffffff/d7d7d7/g;
-            s/aa0000/bc3f39/g;
-            s/ff0000/f0554c/g' ${exp}
-    ${ink} --export-filename=${trg} ${exp} > /dev/null 2>&1
-done
+# Check if inkscape is installed
+[ -z "${no_png}" ] && [ ! -x $(command -v inkscape) ] && echo "Inkscape is not installed" && exit 1
+
+# Exporting our wallpaper
+export_color
+
+echo "Wallpapers have been exported!"
+if [ -n "${opti}" ]; then echo "Optimizing exported pngs. Please wait..."; optipng Matcha/* > /dev/null 2>&1; else echo "Will not optimize pngs"; fi
